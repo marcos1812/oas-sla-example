c5cb2a9e09f0be49419153f16c3f04b02ddacfa8
Marcos1812 <73355855+marcos1812@users.noreply.github.com>
2023-05-12 20:44:52 +0200

    L13 terminado

---
+++ sla-rate-limit/controllers/fosesController.js
@@ -0,0 +1,14 @@
+export function getFosess(req, res) {
+    res.send([{
+        Id:"1",
+        dateNumeric: "1900"
+    }]);
+}
+
+export function findById(req, res) {
+    res.send({
+        Id:"1",
+        dateNumeric: "1900"
+    });
+}
+
